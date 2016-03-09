91e4f98cb0042ebdae8e6a73128c97e30f9e26ce
Denis Kharitonov <Denis@MacBook-Pro-Denis-Kharitonov.local>
2016-03-09 21:52:03 +0300

    initial commit

---
+++ rambler-test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rambler-test
+//
+//  Created by Denis Kharitonov on 09.03.16.
+//  Copyright © 2016 dp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rambler-test/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  rambler-test
+//
+//  Created by Denis Kharitonov on 09.03.16.
+//  Copyright © 2016 dp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
